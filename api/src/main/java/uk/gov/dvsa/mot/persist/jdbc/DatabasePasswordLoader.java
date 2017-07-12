7f851c57bdd99cbc87334dc9ea3e9dc4372e8118
Duncan Eley <duncan.eley@dvsa.gov.uk>
2017-07-12 10:01:38 +0100

    Code review feedback changes.

---
+++ api/src/main/java/uk/gov/dvsa/mot/persist/jdbc/DatabasePasswordLoader.java
@@ -0,0 +1,44 @@
+package uk.gov.dvsa.mot.persist.jdbc;
+
+import com.google.common.base.Strings;
+
+import uk.gov.dvsa.mot.app.ConfigKeys;
+import uk.gov.dvsa.mot.app.ConfigManager;
+
+import java.io.IOException;
+
+public class DatabasePasswordLoader {
+
+    private String passwordDecrypted;
+    private String passwordEncrypted;
+
+    public String getDbPassword() throws IOException {
+
+        String password = null;
+        String configPasswordEncrypted = getEnvironmentVariable(ConfigKeys.DatabaseEncryptedPassword, false);
+
+        if (Strings.isNullOrEmpty(configPasswordEncrypted)) {
+            password = getEnvironmentVariable(ConfigKeys.DatabasePassword);
+
+        } else {
+            if (Strings.isNullOrEmpty(passwordDecrypted) || !configPasswordEncrypted.equalsIgnoreCase(passwordEncrypted)) {
+
+                password = getEnvironmentVariable(ConfigKeys.DatabaseEncryptedPassword);
+
+                passwordEncrypted = configPasswordEncrypted;
+                passwordDecrypted = password;
+            }
+        }
+        return password;
+    }
+
+    String getEnvironmentVariable(String name) throws IOException {
+
+        return ConfigManager.getEnvironmentVariable(name);
+    }
+
+    String getEnvironmentVariable(String name, boolean decrypt) throws IOException {
+
+        return ConfigManager.getEnvironmentVariable(name, decrypt);
+    }
+}
