791f9eeb7a2c8693be91b22f5e11dc5988790ceb
Ryszard Madejski <r.madejski@kainos.com>
2018-08-08 17:57:01 +0200

    BL-8424: hide properties with null values in mot-tests endpoint

--- api/src/main/java/uk/gov/dvsa/mot/trade/api/response/MotTestV2Response.java
+++ api/src/main/java/uk/gov/dvsa/mot/trade/api/response/MotTestV2Response.java
@@ -1,7 +1,10 @@
 package uk.gov.dvsa.mot.trade.api.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class MotTestV2Response extends MotTestResponse {
 
     private String odometerResultType;
