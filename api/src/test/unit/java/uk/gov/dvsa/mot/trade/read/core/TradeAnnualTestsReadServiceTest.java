318b710ab0b6dc67cdb50fd2db420b095b84c807
James Wragg <james.wragg@dvsa.gov.uk>
2019-02-19 09:17:02 +0000

    Tests n stuff

---
+++ api/src/test/unit/java/uk/gov/dvsa/mot/trade/read/core/TradeAnnualTestsReadServiceTest.java
@@ -0,0 +1,13 @@
+package uk.gov.dvsa.mot.trade.read.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TradeAnnualTestsReadServiceTest {
+
+    @Test
+    public void getAnnualTests() {
+
+    }
+}
